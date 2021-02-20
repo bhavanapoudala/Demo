6792b041f86bbf2b0b983abd0ddcab030f60de0f
bhavana <bhavanapoudala@gmail.com>
2021-02-20 11:22:11 +0530

    Added : multiplication of numbers

--- add.sh
+++ add.sh
@@ -1,6 +1,9 @@
-#! /bin/bash -x
+
 a=3
 b=4
-result=$(($a+$b))
-echo $result
+result1=$(($a+$b))
+result2=$(($a*$b))
+echo "Addition of two numbers is" $result1
+echo "Multiplication of two numbers is" $result2
+
 
